ce65e8bd46a3f7e4259b1c0f148625d1541f9b60
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-12-03 02:59:15 -0300

    fix: change auth-fn database

--- docker/mysql/scripts/init.sql
+++ docker/mysql/scripts/init.sql
@@ -1,11 +1,21 @@
-CREATE DATABASE IF NOT EXISTS `testing`;
+CREATE DATABASE IF NOT EXISTS `auth`;
 CREATE DATABASE IF NOT EXISTS `kitchen`;
 CREATE DATABASE IF NOT EXISTS `testing_kitchen`;
 CREATE DATABASE IF NOT EXISTS `order`;
 CREATE DATABASE IF NOT EXISTS `testing_order`;
 
-GRANT ALL PRIVILEGES ON `testing`.* TO 'app'@'%';
+GRANT ALL PRIVILEGES ON `auth`.* TO 'app'@'%';
 GRANT ALL PRIVILEGES ON `kitchen`.* TO 'app'@'%';
 GRANT ALL PRIVILEGES ON `testing_kitchen`.* TO 'app'@'%';
 GRANT ALL PRIVILEGES ON `order`.* TO 'app'@'%';
 GRANT ALL PRIVILEGES ON `testing_order`.* TO 'app'@'%';
+
+CREATE TABLE `auth`.`customers` (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(100) NULL,
+    email VARCHAR(255) NULL,
+    cpf VARCHAR(11) NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    CONSTRAINT customers_pk_index PRIMARY KEY (id)
+)
